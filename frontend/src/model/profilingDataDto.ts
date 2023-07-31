53093c634e6bddedd2f8cb6481fffb3320e2d522
David Rodriguez <davidrbacelar@gmail.com>
2023-07-31 14:31:06 +0200

    Adapt profiling DTO to backend changes

--- frontend/src/model/profilingDataDto.ts
+++ frontend/src/model/profilingDataDto.ts
@@ -10,27 +10,25 @@ export const ProfilingDataDtoSchema = z
 	.object({
 		id: z.string(),
 		status: z.enum(["PENDING", "SUCCESS"]),
-		profiling: z.object({
-			algorithm: ProfilingAlgorithmSchema,
-			time: z.number(),
-			output: z.array(z.object(
-				{
-					id: z.string(),
-					result: z.object(
-						{
-							gender: GenderSchema,
-							age: AgeSchema,
-							fame: FameSchema.optional(),
-							occupation: OccupationSchema.optional(),
-							extroverted: z.number().optional(),
-							stable: z.number().optional(),
-							agreeable: z.number().optional(),
-							conscientious: z.number().optional(),
-							open: z.number().optional(),
-						})
-				}
-			))
-		}).optional().nullable()
+		algorithm: ProfilingAlgorithmSchema,
+		time: z.number().nullable(),
+		output: z.array(z.object(
+			{
+				id: z.string(),
+				result: z.object(
+					{
+						gender: GenderSchema,
+						age: AgeSchema,
+						fame: FameSchema.optional(),
+						occupation: OccupationSchema.optional(),
+						extroverted: z.number().optional(),
+						stable: z.number().optional(),
+						agreeable: z.number().optional(),
+						conscientious: z.number().optional(),
+						open: z.number().optional(),
+					})
+			}
+		)).nullable()
 	})
 
 export type ProfilingDataDto = z.infer<typeof ProfilingDataDtoSchema>;
@@ -38,16 +36,16 @@ export type ProfilingDataDto = z.infer<typeof ProfilingDataDtoSchema>;
 export const toProfilingData = (dto: ProfilingDataDto): ProfilingData => {
 	return {
 		id: dto.id,
-		algorithm: dto.profiling!.algorithm,
-		time: dto.profiling!.time,
-		people: dto.profiling!.output.map((person) => {
+		algorithm: dto.algorithm,
+		time: dto.time!,
+		people: dto.output!.map((person) => {
 			return {
 				name: person.id.toString(),
 				age: person.result.age,
 				gender: person.result.gender,
 				fame: person.result.fame,
 				occupation: person.result.occupation,
-				personalityTraits: dto.profiling!.algorithm === "grivas" ? [
+				personalityTraits: dto.algorithm === "grivas" ? [
 					{ trait: "extroverted", weight: person.result.extroverted! },
 					{ trait: "stable", weight: person.result.stable! },
 					{ trait: "agreeable", weight: person.result.agreeable! },
