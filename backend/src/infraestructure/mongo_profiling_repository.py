0852d29e322c3bb35decb78aac561024841ba482
David Rodriguez <davidrbacelar@gmail.com>
2023-07-31 15:40:44 +0200

    Move mongo connection logic to repo initi

--- backend/src/infraestructure/mongo_profiling_repository.py
+++ backend/src/infraestructure/mongo_profiling_repository.py
@@ -1,26 +1,38 @@
+import logging
+import os
+import pymongo
 from uuid import UUID
-
 from domain.entities.profiling import Profiling
 from domain.repositories.profiling_repository import ProfilingRepository
-from infraestructure.mongo_database import profilings_collection
+
+logger = logging.getLogger("server_logger")
 
 
 class MongoProfilingRepository(ProfilingRepository):
+    def __init__(self):
+        DB_HOST = os.environ.get("DB_HOST", "localhost")
+        mongo_url = f"mongodb://{DB_HOST}:27017"
+
+        client = pymongo.MongoClient(mongo_url, uuidRepresentation="standard")
+
+        database = client["ai-profiler"]
+        self.collection = database["profilings"]
+
     def get_profiling(self, profiling_id: UUID):
-        profiling = profilings_collection.find_one({"_id": profiling_id})
+        profiling = self.collection.find_one({"_id": profiling_id})
         return self.__from_mongo_model(profiling)
 
     def create_profiling(self, profiling: Profiling):
-        profilings_collection.insert_one(self.__to_mongo_model(profiling))
+        self.collection.insert_one(self.__to_mongo_model(profiling))
 
     def update_profiling(self, profiling: Profiling):
-        profiling = profilings_collection.update_one(
+        profiling = self.collection.update_one(
             {"_id": profiling.id},
             {"$set": self.__to_mongo_model(profiling)},
         )
 
     def delete_profiling(self, profiling_id: UUID):
-        profilings_collection.delete_one({"_id": profiling_id})
+        self.collection.delete_one({"_id": profiling_id})
 
     def __to_mongo_model(self, profiling: Profiling):
         return {
